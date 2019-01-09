70932e1a70a1b3711d610b44b0ff140ac0470371
Simon Whale <Simon_Whale@allfleetservices.co.uk>
2019-01-09 20:29:02 +0000

    Adding blank Factory Pattern

---
+++ FactoryPattern/FactoryPattern/FactoryPattern/ConcreteClass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FactoryPattern.Interfaces;
+
+namespace FactoryPattern.FactoryPattern
+{
+    public abstract class ConcreteClass
+    {
+        public abstract IResolve Get();
+    }
+}
---
+++ FactoryPattern/FactoryPattern/FactoryPattern/Factory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FactoryPattern.Interfaces;
+
+namespace FactoryPattern.FactoryPattern
+{
+    public class Factory : ConcreteClass
+    {
+        public override IResolve Get()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
