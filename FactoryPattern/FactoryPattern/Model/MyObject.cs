d4caa80e15bba6f02137f66ae84b65f6cce6854d
Simon Whale <Simon_Whale@allfleetservices.co.uk>
2019-01-11 20:41:42 +0000

    Revert "first draft"
    This reverts commit 7493ec390806c73782cb17979f7fe11cceed9302.

--- FactoryPattern/FactoryPattern/Model/MyObject.cs
+++ FactoryPattern/FactoryPattern/Model/MyObject.cs
@@ -1,18 +1,12 @@
-﻿using FactoryPattern.Interfaces;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace FactoryPattern.Model
 {
-    public sealed class MyObject 
+    public sealed class MyObject
     {
         public int IdValue { get; set; }
         public string Name { get; set; }
-
-        public override string ToString()
-        {
-            return $"ID : {IdValue}, Name:{Name}";
-        }
     }
 }
--- FactoryPattern/FactoryPattern/Program.cs
+++ FactoryPattern/FactoryPattern/Program.cs
@@ -1,6 +1,4 @@
-﻿using FactoryPattern.FactoryPattern;
-using FactoryPattern.Interfaces;
-using System;
+﻿using System;
 
 namespace FactoryPattern
 {
@@ -8,16 +6,7 @@ namespace FactoryPattern
     {
         static void Main(string[] args)
         {
-            Factory myFactoryPattern = new Factory();
-            IResolve mapper = myFactoryPattern.Get("Hi");
-            var mapA = mapper.Resolve();
-            Console.WriteLine(mapA.ToString());
-
-            IResolve mapper5 = myFactoryPattern.Get("Bye");
-            var mapB = mapper5.Resolve();
-            Console.WriteLine(mapB.ToString());
-
-            Console.ReadLine();
+            Console.WriteLine("Hello World!");
         }
     }
 }
